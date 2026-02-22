94dd6521da71a21b71cb6487c75ef999141da08c
Nelson Romaine <nelson@romaine.life>
2026-02-22 09:50:19 -0800

    removed required providers

--- tofu/provider.tf
+++ tofu/provider.tf
@@ -1,11 +1,5 @@
 terraform {
   required_version = ">= 1.6.0"
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 4.0"
-    }
-  }
 }
 
 provider "azurerm" {
